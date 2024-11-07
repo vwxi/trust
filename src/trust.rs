5dcf4613fe11c6970ef70bc531e205317e64fe61
ten <pala@tilde.institute>
2024-11-06 23:58:55 -0500

    yeaahhhh

--- src/trust.rs
+++ src/trust.rs
@@ -8,26 +8,28 @@ type Vector<H> = BTreeMap<H, f64>;
 type Matrix<H> = BTreeMap<H, BTreeMap<H, f64>>;
 
 // a single trust computation
-struct Trust<H: Debug + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> {
+struct Trust<H: Debug + Default + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> {
     // own id
     pub own_id: H,
     // list of pre-trusted peers
     pub pre_trusted_peers: Vec<H>,
     // "C" matrix, peer: { what peer thinks of other peers }
     pub global: Matrix<H>,
-    // initial local vector applied with alpha
-    alpha_local: Vector<H>,
+    // initial local vector
+    initial: Vector<H>,
     // local vector
     pub local: Vector<H>,
+    // global max
+    global_max: f64,
     // alpha value
     pub alpha: f64,
     // delta
     delta: f64,
     // epsilon
-    epsilon: f64
+    epsilon: f64,
 }
 
-impl<H: Debug + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> Trust<H> {
+impl<H: Debug + Default + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> Trust<H> {
     pub fn new(id: H, alpha_: f64, ptp: Vec<H>) -> Self {
         let mut t: Vector<H> = Vector::new();
         let mut t2: Vector<H> = Vector::new();
@@ -35,11 +37,16 @@ impl<H: Debug + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> Trust<H> {
 
         // t(0) = p
         // p_i = 1/|P| if i ∈ P , and p_i = 0 otherwise.
-        let inv_p = 1.0f64 / ptp.len() as f64;
+        let inv_p = if ptp.len() != 0 {
+            1.0f64 / ptp.len() as f64
+        } else {
+            0f64
+        };
+
         for p in ptp.clone() {
             t.entry(p).or_insert(inv_p);
             t2.entry(p).or_insert(inv_p * alpha_);
-            
+
             m.entry(id)
                 .and_modify(|e| {
                     let _ = e.insert(p, inv_p);
@@ -57,11 +64,12 @@ impl<H: Debug + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> Trust<H> {
             own_id: id,
             global: m,
             local: t.clone(),
-            alpha_local: t,
+            initial: t,
             pre_trusted_peers: ptp,
+            global_max: 0f64,
             alpha: alpha_,
             delta: f64::MAX,
-            epsilon: 0.0001f64
+            epsilon: 0.0001f64,
         }
     }
 
@@ -74,7 +82,12 @@ impl<H: Debug + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> Trust<H> {
 
         if i == self.own_id {
             debug!("adding to own entry: {:?} -> {:?} value {}", i, j, score);
-            self.local.entry(j).and_modify(|e| { *e = score; }).or_insert(score);
+            self.local
+                .entry(j)
+                .and_modify(|e| {
+                    *e = score;
+                })
+                .or_insert(score);
         }
 
         debug!("adding to global entry: {:?} -> {:?} value {}", i, j, score);
@@ -92,57 +105,66 @@ impl<H: Debug + Clone + Copy + Ord + PartialOrd + Eq + PartialEq> Trust<H> {
                 t
             });
 
+        self.global_max = self.global.iter().fold(0f64, |a, e| e.1.iter().fold(0.0f64, |a, e| a.max(*e.1)).max(a));
+        
+        debug!("global_max: {}", self.global_max);
+
         true
     }
 
-    pub fn score(&self, i: H, j: H) -> f64 {
-        // If peer i doesn’t download from anybody else,
-        // or if it assigns a zero score to all other peers, cij from Equation 1
-        // will be undefined. In this case, we set cij = pj .
-        if self
-            .global
-            .iter()
-            .map(|e| 0f64.max(if let Some(v) = e.1.get(&j) { *v } else { 0f64 }))
-            .fold(0f64, |a, e| a + e)
-            != 0f64
-        {
-            if let Some(ii) = self.global.get(&i) {
-                // either from global or pj
-                *ii.get(&j).or(self.local.get(&j)).unwrap_or(&0f64)
-            } else {
-                0f64
-            }
+    fn n_score(&self, i: &H, j: &H) -> f64 {
+        // if sum_j max(s_ij, 0) == 0, p_j
+        if self.global_max == 0f64 {
+            *self.initial.get(j).unwrap_or(&0f64)
         } else {
-            0f64
+            // otherwise max(s_ij, 0) / `max`
+            let mut acc = 0f64;
+            if let Some(tvs) = self.global.get(i) {
+                acc = tvs.get(j).unwrap_or(&0f64).max(0f64);
+            }
+
+            dbg!(acc / self.global_max)
         }
     }
 
-    // t(k+1) = (1 − a)CT t(k) + ap 
+    // t(k+1) = (1 − a)CT t(k) + ap
     pub fn iterate(&mut self) {
-        let tk1: Vector<H> = self.global.iter()
-            .filter_map(|(row_idx, row)| {
-                let sum = row
-                    .iter()
-                    .filter_map(|(&col_idx, _)| {
-                        self.local.get(&col_idx)
-                            .map(|local_score| dbg!(self.score(*row_idx, col_idx)) * dbg!(local_score))
-                    })
-                    .sum::<f64>() * (1.0f64 - self.alpha) + self.alpha_local.get(row_idx).or(Some(&0f64)).unwrap();
+        let mut tk1: Vector<H> = Vector::new();
 
-                if sum != 0.0 {
-                    Some((*row_idx, sum))
-                } else {
-                    None
+        // (c_i1*t_1) + (c_i2*t_2) + ... + (c_in+t_n)
+        for (i, trusters) in &self.global {
+            for (j, trust_value) in trusters {
+                if i != j {
+                    let t = self.n_score(i, j) * dbg!(trust_value);
+                    debug!("computing trust from {:?} to {:?} : {}", i, j, t);
+
+                    tk1.entry(*j).and_modify(|e| { *e += t; }).or_insert(t);
                 }
-            })
-            .collect();
+            }
+        }
 
-        self.delta = tk1.iter().filter_map(|(row_idx, row)| {
-            Some((row - self.local.get(row_idx).or(Some(&0f64)).unwrap_or(&0f64)).powf(2.0f64))
-        }).sum::<f64>().sqrt();
+        // (1 − a)CT t(k)
+        tk1.iter_mut().for_each(|(en, e)| {
+            *e += self.alpha * self.initial.get(en).unwrap_or(&0.0f64);
+        });
+
+        let mag = tk1.iter().fold(0.0f64, |a, e| a + e.1.powf(2.0f64)).sqrt();
+
+        // normalize vector
+        tk1.iter_mut().for_each(|(_, e)| {
+            *e /= mag;
+        });
+
+        self.delta = tk1
+            .iter()
+            .map(|(row_idx, row)| {
+                (row - self.local.get(row_idx).or(Some(&0f64)).unwrap_or(&0f64)).powf(2.0f64)
+            })
+            .sum::<f64>()
+            .sqrt();
 
         self.local.clear();
-        self.local = tk1;
+        self.local = dbg!(tk1);
 
         debug!("current delta: {}, epsilon: {}", self.delta, self.epsilon);
     }
@@ -165,25 +187,20 @@ mod tests {
 
     #[traced_test]
     #[test]
-    fn trust1() {
-        let mut trust: Trust<U256> = Trust::new(U256::from(1), 0.001, vec![]);
-        
-        // A
-        trust.add(U256::from(1), U256::from(2), 0.2);
-        trust.add(U256::from(1), U256::from(3), 0.3);
-        trust.add(U256::from(1), U256::from(4), 0.1);
+    fn equal() {
+        let mut trust: Trust<U256> = Trust::new(U256::from(1), 0.95, vec![]);
 
-        // B
-        trust.add(U256::from(2), U256::from(3), 0.2);
-        trust.add(U256::from(2), U256::from(4), 0.2);
+        trust.add(U256::from(1), U256::from(2), 1.0f64);
+        trust.add(U256::from(1), U256::from(3), 1.0f64);
+        trust.add(U256::from(2), U256::from(1), 1.0f64);
+        trust.add(U256::from(2), U256::from(3), 1.0f64);
+        trust.add(U256::from(3), U256::from(1), 1.0f64);
+        trust.add(U256::from(3), U256::from(2), 1.0f64);
 
-        // C
-        trust.add(U256::from(3), U256::from(1), 0.4);
-
-        // D
-        trust.add(U256::from(4), U256::from(2), 1.0);
-    
-        
         trust.run();
+
+        assert_eq!(trust.local.get(&U256::from(1)).unwrap(), trust.local.get(&U256::from(2)).unwrap());
+        assert_eq!(trust.local.get(&U256::from(2)).unwrap(), trust.local.get(&U256::from(3)).unwrap());
+        assert_eq!(trust.local.get(&U256::from(1)).unwrap(), trust.local.get(&U256::from(3)).unwrap());
     }
 }
